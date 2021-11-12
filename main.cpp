a9b9f54cc9088658856af0f2b5d0f640d436b59d
iCAROS7 <hominlab@gmail.com>
2021-11-13 01:41:26 +0900

    주석 수정 및 isFail 변수 제거

--- main.cpp
+++ main.cpp
@@ -204,18 +204,16 @@ int main() {
         list.push_back(new Treasure(distr(gen), distr(gen)));       // 보물 객체 랜덤 위치 생성
         list.push_back(new Enemy(distr(gen), distr(gen)));          // 괴물 객체 랜덤 위치 생성
         list.push_back(new Trap(distr(gen), distr(gen)));           // 함정 객체 랜덤 위치 생성
-    } while (list[0]->checkCollision(list[1], list[2], list[3]));     // 각자의 위치가 겹치지 않을 때 탈출
+    } while (list[0]->checkCollision(list[1], list[2], list[3]));   // 각자의 위치가 겹치지 않을 때 탈출
 
-    system("cls"); // 콘솔 출력창 초기화
+    system("cls");      // 콘솔 출력창 초기화
     cout << "이 게임의 목표는 함정(T)이나 괴물(E)를 만나지 않고 보물(T)에 도달 하는 것이다.";
     cout << "함정과 보물은 같은 문자 (T)로 표현 되므로 50%의 확률로 신중하게 선택을 해야한다.\n" << endl;
     cout << "주인공은 a, s, d, w키를 이용하여 움직일 수 있다.\n" << endl;
+    system("pause");    // press key to continue
 
-    system("pause"); // press key to continue
-
-    bool isFail; // 게임 실패 유무 확인
-    do {
-        system("cls"); // 콘솔 출력창 초기화
+    while (true) {
+        system("cls");  // 콘솔 출력창 초기화
 
         board.clearBoard();
         for (auto& e : list) { board.setValue(e->getY(), e->getX(), e->getShape()); }
@@ -233,7 +231,7 @@ int main() {
         if (direction == 'c') { break; }
         for (auto& e : list) { e->move(direction); }
 
-        // TODO: isFail 유무를 통하여 단순 break; 가 아닌 다음 스테이 진행시 활용하기
+        // TODO: break가 아닌 다음 스테이 진행시 활용하기
         if (list[0]->checkCollision(list[1])) {
             printResult(0);
             break;
@@ -246,9 +244,7 @@ int main() {
             printResult(1);
             break;
         }
-
-        isFail = false;
-    } while (!isFail);
+    };
 
     for (auto& e : list) { delete e; }
     list.clear();
